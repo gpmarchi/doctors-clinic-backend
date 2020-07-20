429dfc418f6fee7966c18643d4f0a7e31d3aafe1
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-07-20 11:33:12 -0300

    remove unused relation to exam

--- app/Models/ExamRequest.js
+++ app/Models/ExamRequest.js
@@ -4,10 +4,6 @@
 const Model = use('Model')
 
 class ExamRequest extends Model {
-  exam() {
-    return this.belongsTo('App/Models/Exam', 'exam_id', 'id')
-  }
-
   consultation() {
     return this.belongsTo('App/Models/Consultation', 'consultation_id', 'id')
   }
